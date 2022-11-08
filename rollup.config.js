958e9ede9e04db629039b73f89e23fd9ca76552b
MoonCheung <ikmoons.is@gmail.com>
2022-11-08 23:24:40 +0800

    chore: new lib folder

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import path from 'path'
+import pkg from './package.json';
+// import json from '@rollup/plugin-json';
+// import esbuild from 'rollup-plugin-esbuild';
+// import nodeResolve from '@rollup/plugin-node-resolve';
+
+export default {
+	input: path.resolve(__dirname, './src/index.js'),
+	output: [
+		{ file: path.resolve(__dirname, pkg.main), format: 'umd', name: 'index' },
+		{ file: path.resolve(__dirname, pkg.module), format: 'esm', name: 'index' }
+	]
+};
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+import number from '../src/index';
+
+describe('go the-answer', () => {
+  test('是否相等', () => {
+    expect(number).toEqual(31);
+  });
+})
